7f911d55b743ef4f777e8fc15896e0a2d42f67df
ealejandrootalvaro <ealejandro.otalvaro@gmail.com>
2016-11-04 14:57:40 -0500

    Update Constantes.hs

--- src/Constantes.hs
+++ src/Constantes.hs
@@ -14,7 +14,7 @@ connectionStr :: ByteString
 connectionStr = "postgres://ndvmdfyexshzni:bXvG8_ldqY9bGEMvDK9qY-tZvF@ec2-54-235-207-226.compute-1.amazonaws.com:5432/de2if8v7djdfmi"
 
 puerto :: Int
-puerto = 8087
+puerto = 8080
 
 mensajeEmail :: String
 mensajeEmail = "Su cuenta se autodestruira en 5 segundos,\n\nIngrese con la siguiente contraseña para detener la explosion.\n\nContraseña: "
